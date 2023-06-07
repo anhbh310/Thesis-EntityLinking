d6cfb9618e29267d313668ff44862553109c345f
anh <anhbh.310@gmail.com>
2023-06-07 23:19:38 +0700

    flask linking API is implemented

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from utils.entity_generator import *
 
 from wordEmbedding.bert import get_word_embedding, get_word_embedding_from_doc, reformat_entity_mention
 
+from flask import Flask, Response, request, jsonify
+
+app = Flask(__name__)
+
 cos = torch.nn.CosineSimilarity(dim=0)
 
 
@@ -35,14 +39,32 @@ def ranking_process(r_entity_mention, in_doc):
                 ranking_ret.append((page, calculate_similar(src_embed, candidate_embed)))
     return ranking_ret
 
+@app.route('/el', methods=['GET', 'POST'])
+def handle_entity_linking():
+    def find_most_relevant_entity(entity_mention, in_doc):
+        ret = ranking_process(r_entity_mention=entity_mention, in_doc=in_doc)
+        result = None
+        for c in ret:
+            if result is None or c[1] > result[1]:
+                result = c
+        return result
+    
+    data = request.json
+    mention, doc = data["entity_mention"], data["in_doc"]
+    # print("----{}---------{}".format(mention, doc))
+    ret_entity = find_most_relevant_entity(entity_mention=mention, in_doc=doc)
+    return jsonify({"ret": {"entity_name": ret_entity[0][0],
+                            "url": "http://vi.wikipedia.org/?curid={}".format(ret_entity[0][1])}}), 200
 
 if __name__ == '__main__':
     # Input
     # ret = ranking_process(r_entity_mention="Cẩm Phả", in_doc="Bridgestone: Đưa trách nhiệm cộng đồng vào văn hóa doanh nghiệp.Trong không khí tất bật trước Tết, Bridgestone vẫn tâm huyết với hoạt động chăm sóc lốp và xe để khách hàng tại Cẩm Phả di chuyển an toàn qua chương trình 'Bridgestone Lăn bánh an toàn'.Diễn ra trong ba ngày từ 19 - 21/1/2018, hơn 100 ô tô đã được kiểm tra và chăm sóc tại B-select Hải Anh, 36 Lê Thanh Nghị, Cẩm Bình, Cẩm Phả.")
-    ret = ranking_process(r_entity_mention="Hồ Chí Minh", in_doc="Thành phố Hồ Chí Minh hay Sài Gòn, là thành phố lớn nhất Việt Nam và là một siêu đô thị trong tương lai gần. Đây còn là trung tâm kinh tế, văn hóa, giải trí và giáo dục tại Việt Nam. Thành phố Hồ Chí Minh là thành phố trực thuộc trung ương thuộc loại đô thị đặc biệt của Việt Nam.")
-    print(ret)
-    result = None
-    for c in ret:
-        if result is None or c[1] > result[1]:
-            result = c
-    print(result)
+    # ret = ranking_process(r_entity_mention="Hồ Chí Minh", in_doc="Thành phố Hồ Chí Minh hay Sài Gòn, là thành phố lớn nhất Việt Nam và là một siêu đô thị trong tương lai gần. Đây còn là trung tâm kinh tế, văn hóa, giải trí và giáo dục tại Việt Nam. Thành phố Hồ Chí Minh là thành phố trực thuộc trung ương thuộc loại đô thị đặc biệt của Việt Nam.")
+    # print(ret)
+    # result = None
+    # for c in ret:
+    #     if result is None or c[1] > result[1]:
+    #         result = c
+    # print(result)
+
+    app.run(host='0.0.0.0', port=8091)
